086c4841dd0b095f328cd7ddf4047d2d0cdf93da
Max <78656849+MXML11LN@users.noreply.github.com>
2021-03-13 11:18:01 +0200

    перенесли сторінку магазину на реакт

--- src/index.js
+++ src/index.js
@@ -1,49 +1,5 @@
 import React from "react"
 import ReactDom from "react-dom"
-
-//const h1 = React.createElement("h1",{
-//  id:"test"
-//},"Hello world");
-
-//jsx
-//const h1 = <h1 id="test 2">Hello world 2</h1>
-//const ul= <ul>
-//  <li>list item 1 </li>
-//  <li>list item 2 </li>
-//</ul>
-
-//const app =<div>
-//  {h1}
-//  {ul}
-//</div>
-
-const Header = () =>{
-  return(
-    <>
-      <h1>helo world</h1>
-    </>
-  )
-}
-const Content = () =>{
-  return(
-    <>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, numquam non? Officiis ex dicta explicabo suscipit. Tempora quisquam, quidem nesciunt tenetur minus excepturi consequatur nobis, esse eaque nemo ratione sit?</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, numquam non? Officiis ex dicta explicabo suscipit. Tempora quisquam, quidem nesciunt tenetur minus excepturi consequatur nobis, esse eaque nemo ratione sit?</p>
-    </>
-  )
-}
-
-function App() {
-  return(
-<>
-
-<Header/>
-<Content/>
-
-</>
-
-  )
-}
-
+import App from "./App"
 
 ReactDom.render(<App/>,document.getElementById("root"))
