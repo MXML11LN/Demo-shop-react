acd9527939b61a4c95421a6de2153b3ea505062b
Max <78656849+MXML11LN@users.noreply.github.com>
2021-03-19 15:56:31 +0200

    added product-list-item-css

--- src/App/Main/Products/ProductList.js
+++ src/App/Main/Products/ProductList.js
@@ -5,6 +5,11 @@ const ProductList =() => {
     return(
         <>
         <h1 className = "page-title">Product List</h1>
+        <div className = "row">
+            <div className = "col-lg-6">
+                <ProductListItem/>
+            </div>
+        </div>
         </>
     )
 }
--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -1,14 +1,16 @@
 import React from "react"
 
+import './product-list-item.css'
+
 const ProductListItem = () =>{
     return(
-        <div className="product- list-item">
+        <div className="product-list-item">
             <div className="product-title">iPhonex</div>
             <div className="product-description">This is iPhone X</div>
             <div className="product-features">Type: phone</div>
             <div className="product-features">capacity: 64gb</div>
             <div className="product-price">$ 800X</div>
-            <button className=""btn-add-to-cart>Add to cart</button>
+            <button className="btn-add-to-cart">Add to cart</button>
         </div>
         )
 }
