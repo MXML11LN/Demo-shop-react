55ab654bd47e854784bc96bbd195f82153a53db9
Max <78656849+MXML11LN@users.noreply.github.com>
2021-03-20 10:36:25 +0200

    add destructurization to product list item

--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -2,15 +2,34 @@ import React from "react"
 
 import './product-list-item.css'
 
-const ProductListItem = (props) =>{
-    console.log(props)
+/* деструктуризаціяж: массив розбивається на змінні */
+const user ={
+    name:"Ivan",
+    age: 20,
+}
+
+const{
+    name,
+    age,
+} = user;
+
+console.log(name);
+
+
+const ProductListItem = ({
+    name,
+    description,
+    type,
+    capacity,
+    price,
+}) =>{
     return(
         <div className="product-list-item">
-            <div className="product-title">{props.name}</div>
-            <div className="product-description">{props.description}</div>
-            <div className="product-features">Type: {props.type}</div>
-            <div className="product-features">Capacity: {props.capacity} GB</div>
-            <div className="product-price"> $ {props.price}</div>
+            <div className="product-title">{name}</div>
+            <div className="product-description">{description}</div>
+            <div className="product-features">Type: {type}</div>
+            <div className="product-features">Capacity: {capacity} GB</div>
+            <div className="product-price"> $ {price}</div>
             <button className="btn-add-to-cart">Add to cart</button>
         </div>
         )
