4881ff64324a8d6c8e966b9d3b69e473ab0b4cd1
Max <78656849+MXML11LN@users.noreply.github.com>
2021-03-21 10:37:21 +0200

    add react count with react hooks

--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -25,6 +25,9 @@ const ProductListItem = ({
     price,
     img,
 }) =>{
+
+    const [count, setCount] = useState(1);
+
     return(
         <div className="product-list-item">
             <div className = "product-img">
@@ -34,6 +37,11 @@ const ProductListItem = ({
             <div className="product-description">{description}</div>
             <div className="product-features">Type: {type}</div>
             <div className="product-features">Capacity: {capacity} GB</div>
+            <div className="product-quantity"> 
+            <button onClick={() => setCount(count-1)}>-</button>
+            <input type="text" value={count} readOnly/>
+            <button onClick={() => setCount(count+1)}>+</button>
+            </div>
             <div className="product-price"> $ {price}</div>
             <button className="btn-add-to-cart">Add to cart</button>
         </div>
@@ -50,5 +58,7 @@ ProductListItem.defaultProps ={
     description:" no description ... ",
     img:"images/default-img.png",
 }
+
+
 
 export default ProductListItem
