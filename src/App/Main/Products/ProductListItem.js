353a3ba78c7a8672c993a12ef51dbaa188bb1cdd
Max <78656849+MXML11LN@users.noreply.github.com>
2021-03-21 10:51:09 +0200

    changed functional component to class component

--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -1,33 +1,18 @@
-import React from "react"
+import React, { Component } from "react"
 import PropTypes from 'prop-types'
 
 import './product-list-item.css'
 
-/* деструктуризаціяж: массив розбивається на змінні
-const user ={
-    name:"Ivan",
-    age: 20,
-}
-
-const{
-    name,
-    age,
-} = user;
-
-console.log(name); */
-
-
-const ProductListItem = ({
-    name,
-    description,
-    type,
-    capacity,
-    price,
-    img,
-}) =>{
-
-    const [count, setCount] = useState(1);
-
+class ProductListItem extends Component{
+    render(){
+        const{
+            img,
+            name,
+            description,
+            type,
+            capacity,
+            price
+        } = this.props;
     return(
         <div className="product-list-item">
             <div className = "product-img">
@@ -38,14 +23,15 @@ const ProductListItem = ({
             <div className="product-features">Type: {type}</div>
             <div className="product-features">Capacity: {capacity} GB</div>
             <div className="product-quantity"> 
-            <button onClick={() => setCount(count-1)}>-</button>
-            <input type="text" value={count} readOnly/>
-            <button onClick={() => setCount(count+1)}>+</button>
+            <button>-</button>
+            <input type="text" value="1" readOnly/>
+            <button>+</button>
             </div>
             <div className="product-price"> $ {price}</div>
             <button className="btn-add-to-cart">Add to cart</button>
         </div>
         )
+}
 }
 ProductListItem.propTypes = {
     name:PropTypes.string.isRequired,
