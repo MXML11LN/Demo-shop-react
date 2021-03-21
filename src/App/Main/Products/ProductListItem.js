dc5bceccb7bba395739665a7425c1ce435eab649
Max <78656849+MXML11LN@users.noreply.github.com>
2021-03-21 11:12:24 +0200

    add product count throu arrow function

--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -4,6 +4,17 @@ import PropTypes from 'prop-types'
 import './product-list-item.css'
 
 class ProductListItem extends Component{
+
+    state ={
+        productCount:1,
+    }
+
+    onIncrementClick = () => {
+        this.setState((prevState) =>({
+            productCount:prevState.productCount +1
+        }))
+    }
+
     render(){
         const{
             img,
@@ -13,6 +24,7 @@ class ProductListItem extends Component{
             capacity,
             price
         } = this.props;
+        console.log(this)
     return(
         <div className="product-list-item">
             <div className = "product-img">
@@ -24,8 +36,8 @@ class ProductListItem extends Component{
             <div className="product-features">Capacity: {capacity} GB</div>
             <div className="product-quantity"> 
             <button>-</button>
-            <input type="text" value="1" readOnly/>
-            <button>+</button>
+            <input type="text" value={this.state.productCount} readOnly/>
+            <button onclick={this.onIncrementClick}>+</button>
             </div>
             <div className="product-price"> $ {price}</div>
             <button className="btn-add-to-cart">Add to cart</button>
