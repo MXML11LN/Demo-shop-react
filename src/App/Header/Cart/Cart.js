0ff48e855e820a03bfbe10e37460101a91f5d013
Max <78656849+MXML11LN@users.noreply.github.com>
2021-04-08 14:45:09 +0300

    add to cart by product name

--- src/App/Header/Cart/Cart.js
+++ src/App/Header/Cart/Cart.js
@@ -1,6 +1,7 @@
 
 import React from "react"
 import {keys} from "lodash"
+import products from "../../Main/Products/products"
 import "./cart.css"
 
 const Cart = ({
@@ -11,7 +12,7 @@ const Cart = ({
         <div className="cart text-center">
 		    {
                 keys(productsInCart).map(id=>(
-                   <div key={id}>{id}:{productsInCart[id]}</div> 
+                   <div key={id}>{products[id - 1].name} : {productsInCart[id]}</div> 
                 ))
             }
         </div>
